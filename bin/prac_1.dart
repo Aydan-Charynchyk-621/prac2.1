dfa11823770e5d73d83774f827a658dde4868c7e
Aydan <ajdancaryncyk@gmail.com>
2022-11-12 02:46:25 +0700

    First commit. Create console app

---
+++ bin/prac_1.dart
@@ -0,0 +1,5 @@
+import 'package:prac_1/prac_1.dart' as prac_1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${prac_1.calculate()}!');
+}
---
+++ test/prac_1_test.dart
@@ -0,0 +1,8 @@
+import 'package:prac_1/prac_1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
