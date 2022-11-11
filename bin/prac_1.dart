928335dc381f4ec378ec2d98c0b4e403e75e4834
Чарынчык Айдан Валерьевич <117069700+Aydan-Charynchyk-621@users.noreply.github.com>
2022-11-12 02:47:01 +0700

    Update prac_1.dart

--- bin/prac_1.dart
+++ bin/prac_1.dart
@@ -1,5 +1,35 @@
-import 'package:prac_1/prac_1.dart' as prac_1;
+import 'dart:io';
+import 'dart:math';
 
-void main(List<String> arguments) {
-  print('Hello world: ${prac_1.calculate()}!');
-}
+void main() {
+
+//
+//
+//Задание 3
+//
+//print("Введите число-");
+//int a = int.parse(stdin.readLineSync()!);
+//if (a != 0 && a % 10 == 0) {
+//print("Да");
+//}
+//else {
+//print("Нет");
+//}
+//}
+//
+//
+//Задание 4
+//
+//
+//int a = 1;
+//int summ = 0;
+//print("Введите число-");
+//int c = int.parse(stdin.readLineSync()!);
+//while (a > 0){
+//a = int.parse(stdin.readLineSync()!);
+//if (a % c == 0) {
+//summ += a;
+//}
+//}
+//print(summ);
+//}
