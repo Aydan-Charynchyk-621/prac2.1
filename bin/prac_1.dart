6762639cc1899f2d4fae45d3dd494135e84f8b7d
Чарынчык Айдан Валерьевич <117069700+Aydan-Charynchyk-621@users.noreply.github.com>
2022-11-12 09:20:34 +0700

    Update prac_1.dart

--- bin/prac_1.dart
+++ bin/prac_1.dart
@@ -3,33 +3,34 @@ import 'dart:math';
 
 void main() {
 
-//
-//
+
+
 //Задание 3
-//
-//print("Введите число-");
-//int a = int.parse(stdin.readLineSync()!);
-//if (a != 0 && a % 10 == 0) {
-//print("Да");
-//}
-//else {
-//print("Нет");
-//}
-//}
-//
+/*
+print("Введите число-");
+int a = int.parse(stdin.readLineSync()!);
+if (a != 0 && a % 10 == 0) {
+print("Да");
+}
+else {
+print("Нет");
+}
+}
+/*
 //
 //Задание 4
 //
-//
-//int a = 1;
-//int summ = 0;
-//print("Введите число-");
-//int c = int.parse(stdin.readLineSync()!);
-//while (a > 0){
-//a = int.parse(stdin.readLineSync()!);
-//if (a % c == 0) {
-//summ += a;
-//}
-//}
-//print(summ);
-//}
+/*
+int a = 1;
+int summ = 0;
+print("Введите число-");
+int c = int.parse(stdin.readLineSync()!);
+while (a > 0){
+a = int.parse(stdin.readLineSync()!);
+if (a % c == 0) {
+summ += a;
+}
+}
+print(summ);
+}
+/*
